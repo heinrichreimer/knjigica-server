96beb5146b00756d77b7545620a197201346befb
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2018-11-20 09:57:31 +0100

    Add language parsing to OperaLib parser, add OperaLib parser test logger

--- parser/src/main/java/de/unihalle/informatik/bigdata/knjigica/parser/OperaLibLibrettoParser.kt
+++ parser/src/main/java/de/unihalle/informatik/bigdata/knjigica/parser/OperaLibLibrettoParser.kt
@@ -1,37 +1,50 @@
 package de.unihalle.informatik.bigdata.knjigica.parser
 
 import de.unihalle.informatik.bigdata.knjigica.data.Libretto
+import de.unihalle.informatik.bigdata.knjigica.util.languageRange
 import org.jsoup.Jsoup
 import java.io.File
+import java.util.*
 
 
 object OperaLibLibrettoParser : Parser<File, Libretto> {
     override suspend fun parse(source: File): Libretto {
         val document = Jsoup.parse(source, Charsets.UTF_8.name())
 
-        val languageScript = document.select("head > script[type=\"text/javascript\"]").last().text()
-        val language
+        val languageScript = document.head()
+                .select("script[type=\"text/javascript\"]")
+                .last()
+                .html()
+        val languageScriptRegex: Regex = Regex("menuEUrid\\( ?'([a-z]{3})' ?\\) ?;")
+        val languageString = languageScript.lineSequence()
+                .map(String::trim)
+                .firstOrNull { it matches languageScriptRegex }
+                ?.replace(languageScriptRegex) { result ->
+                    result.groups[1]?.value ?: ""
+                }
+                ?.takeIf(String::isNotBlank)
+        val language = when (languageString) {
+            "fra" -> Locale.FRENCH.languageRange
+            "deu" -> Locale.GERMAN.languageRange
+            "eng" -> Locale.ENGLISH.languageRange
+            "rus" -> Locale.LanguageRange("ru")
+            else -> Locale.ITALIAN.languageRange
+        }
+        println("language: ${language.range}")
 
         val title = document.select("h2")[0].text()
+        println("title: $title")
 
         val ridInfos = document.select("p[class=\"rid_info\"]")
-        val subtitle = ridInfos[0].text()
+        val subtitle = ridInfos[0].text().lineSequence().firstOrNull()
+        println("subtitle: $subtitle")
 
         val ridInfoBolds = ridInfos[1].select(" > b")
         val textAuthor = ridInfoBolds[0].text()
+        println("textAuthor: $textAuthor")
         val musicAuthor = ridInfoBolds[1].text()
+        println("musicAuthor: $musicAuthor")
 
-        val author = document.select("p[class=\"rid_info\"] > b")
-        val note = document.select
-        val premiere.date = document.select
-        val premiere.place = document.select
-        val roles.name = document.select
-        val roles.description = document.select
-        val roles.voice = document.select
-        val plot.Section.section = document.select
-        val plot.Section.level = document.select
-        val plot.Text.role = document.select
-        val plot.Text.text = document.select
-        val plot.Instruction.instruction = document.select
+        TODO()
     }
 }
---
+++ parser/src/test/java/de/unihalle/informatik/bigdata/knjigica/LogOperaLibParser.kt
@@ -0,0 +1,25 @@
+package de.unihalle.informatik.bigdata.knjigica
+
+import de.unihalle.informatik.bigdata.knjigica.parser.OperaLibLibrettoParser
+import kotlinx.coroutines.runBlocking
+import java.io.File
+import kotlin.random.Random
+
+object LogOperaLibParser {
+    private val parser = OperaLibLibrettoParser
+    private val path = "corpus/crawl/html/opera_lib_libretto/rid.html"
+    val random = Random(path.hashCode())
+
+    @JvmStatic
+    fun main(vararg args: String) {
+        runBlocking {
+            val index = 0// random.nextInt(0, 433)
+            val indexedPath = path +
+                    if (index > 0) ".$index" else ""
+            val file = File(indexedPath)
+            println(file.absolutePath)
+
+            parser.parse(file)
+        }
+    }
+}
