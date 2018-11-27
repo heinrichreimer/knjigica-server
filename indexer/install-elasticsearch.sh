2177a966a57699560340580a739ec4e3369e7517
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2018-11-27 14:27:21 +0100

    Update elastic search version, add parser dependency to indexer

--- indexer/install-elasticsearch.sh
+++ indexer/install-elasticsearch.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # If not installed install JDK/JRE < 9
+ELASTIC_VERSION=6.5.1
 
 # Download ElasticSearch.
-curl -L -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.6/elasticsearch-2.4.6.tar.gz
+curl -L -O "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ELASTIC_VERSION.tar.gz"
 # Extract downloaded sources.
-tar -xvf elasticsearch-2.4.6.tar.gz
+tar -xvf "elasticsearch-$ELASTIC_VERSION.tar.gz"
