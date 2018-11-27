ffff67408c3d59e00c55c305fdad8f751fc6dc01
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2018-11-27 14:28:46 +0100

    Make test path constant

--- parser/src/test/java/de/unihalle/informatik/bigdata/knjigica/LogOperaLibParser.kt
+++ parser/src/test/java/de/unihalle/informatik/bigdata/knjigica/LogOperaLibParser.kt
@@ -7,15 +7,14 @@ import kotlin.random.Random
 
 object LogOperaLibParser {
     private val parser = OperaLibLibrettoParser
-    private val path = "corpus/crawl/html/opera_lib_libretto/rid.html"
+    private const val path = "corpus/crawl/html/opera_lib_libretto/rid.html"
     private val random = Random.Default
 
     @JvmStatic
     fun main(vararg args: String) {
         runBlocking {
             val index = random.nextInt(0, 433)
-            val indexedPath = path +
-                    if (index > 0) ".$index" else ""
+            val indexedPath = path + if (index > 0) ".$index" else ""
             val file = File(indexedPath)
             println(file.absolutePath)
 
