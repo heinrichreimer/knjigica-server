b8a45680cc8894ef23443ac7d16e32d57391cc66
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-01-21 22:09:00 +0100

    Add test logging in converter

--- parser/src/main/java/de/unihalle/informatik/bigdata/knjigica/parser/architecture/Converter.kt
+++ parser/src/main/java/de/unihalle/informatik/bigdata/knjigica/parser/architecture/Converter.kt
@@ -1,6 +1,5 @@
 package de.unihalle.informatik.bigdata.knjigica.parser.architecture
 
-import de.unihalle.informatik.bigdata.knjigica.parser.util.parallelForEach
 import okio.*
 import java.io.File
 
@@ -14,17 +13,26 @@ abstract class Converter<T> {
         val inputFiles = inputDirectory
                 .listFiles()
                 .asIterable()
-        inputFiles.parallelForEach { input ->
+        inputFiles.forEach { input ->
+            //FIXME Use parallelForEach again.
+            println("Opening source buffer for file '${input.path}'.")
             val source = input
                     .source()
                     .buffer()
+            println("Parsing.")
             val data = parser.parse(source)
 
             val output = File(outputDirectory, namingStrategy(data, input))
+            println("Opening sink buffer for file '${output.path}'.")
             val sink = output
                     .sink()
                     .buffer()
+            println("Formatting.")
             formatter.format(sink, data)
+            println("Done.")
+            output.useLines { it.forEach { println(it) } }
+            println("Printed.")
+            // FIXME This cuts off the JSON at some point.
         }
     }
 }
