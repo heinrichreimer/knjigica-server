88dde0ba470918d54946cd545fe51171b1d3a5a6
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-01-21 22:01:11 +0100

    Delete web client module

--- client-web/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import {Router} from "express";
-
-/* GET home page. */
-export let router = Router()
-    .get('/', (req, res) => {
-        res.render('index', { title: 'Express' });
-    });
--- client-web/src/routes/users.ts
+++
@@ -1,7 +0,0 @@
-import {Router} from "express";
-
-/* GET users listing. */
-export let router = Router()
-    .get('/', (req, res) => {
-        res.send('respond with a resource');
-    });
