cd5f2b4b3f166286c24841c60cca4bc900a7c436
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-01-28 12:53:08 +0100

    Improve parallel collection operations

--- parser/src/main/java/de/unihalle/informatik/bigdata/knjigica/parser/util/CoroutineExtensions.kt
+++ parser/src/main/java/de/unihalle/informatik/bigdata/knjigica/parser/util/CoroutineExtensions.kt
@@ -4,19 +4,18 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.joinAll
-import kotlin.coroutines.coroutineContext
 
 suspend fun <A, B> Iterable<A>.parallelMap(transform: suspend (A) -> B): List<B> {
     return map {
-        GlobalScope.async(coroutineContext) {
+        GlobalScope.async {
             transform(it)
         }
     }.awaitAll()
 }
 
-suspend fun <A> Iterable<A>.parallelForEach(transform: suspend (A) -> Unit): Unit {
+suspend fun <A> Iterable<A>.parallelForEach(transform: suspend (A) -> Unit) {
     return map {
-        GlobalScope.async(coroutineContext) {
+        GlobalScope.async {
             transform(it)
         }
     }.joinAll()
